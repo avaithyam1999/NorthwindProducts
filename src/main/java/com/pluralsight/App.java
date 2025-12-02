c6357dd7d670f5d6cb833272e9eb761e4463c9a8
AJ V <avaithyam@yahoo.com>
2025-12-02 11:50:35 -0500

    better commit eh

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -5,22 +5,44 @@ import java.sql.*;
 public class App {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         String url = "jdbc:mysql://localhost:3306/northwind";
-        String username = "root";
-        String password = "yearup24";
+        String username = args[0];
+        String password = args[1];
 
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection connection = DriverManager.getConnection(url, username, password);
-        Statement statement = connection.createStatement();
 
-        String query = "select * from products;";
+        String query = """
+                select productID, productName, unitPrice, UnitsInStock
+                from products
+                where productName like ? or supplierID like ?;
+                """;
 
-        ResultSet results = statement.executeQuery(query);
+        PreparedStatement statement = connection.prepareStatement(query);
+
+        String productName = "%hi%";
+        int supplierID = 4;
+
+        statement.setString(1, productName);
+        statement.setInt(2, supplierID);
+        ResultSet results = statement.executeQuery();
 
         while (results.next()) {
-            String name = results.getString("ProductName");
-            System.out.println(name);
+            String name = results.getString(1);
+            productName = results.getString(2);
+            int unitPrice = results.getInt(3);
+            int unitsInStock = results.getInt(4);
+            System.out.printf("""
+                    
+                    Product ID: %s
+                    Product Name: %s
+                    Unit Price: %d
+                    Units in Stock: %d
+                    
+                    """, name, productName, unitPrice, unitsInStock);
         }
 
+        results.close();
+        statement.close();
         connection.close();
     }
 }
---
+++ src/main/java/com/pluralsight/CommandLineExercise.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+
+public class CommandLineExercise {
+
+    public static void main(String[] args) {
+        String firstName = args[0];
+        String lastName = args[1];
+        System.out.println("Hi, my name is, what? My name is, who? / My name is, chka-chka, " + firstName + " " + lastName);
+    }
+}
